3a4dc553160a02a7af3a2ddbb0d6bb433a4c2e20
jiehua <1753970025@qq.com>
2015-11-20 15:06:25 +0800

    独立clip-tkinter

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 ### 截图支持
 
-剪贴板操作默认使用`python`内置类库`Thinter`，仅支持纯文本共享；如果需要共享剪贴板截图，需要安装第三方类库`pygtk`:
+`clip-tkinter.py`：使用`python`内置类库`Thinter`，仅支持纯文本共享，目前性能存在比较大的问题；
+
+`clip-pygtk.py`：支持截图共享（`推荐`），需要安装第三方类库`pygtk`，
 
 * Ubuntu
 
@@ -16,14 +18,20 @@
 
 ### 运行
 
-假设`电脑A`的内网IP为：`172.16.2.100`，绑定端口：`34567`；`电脑B`的内网IP为：`172.16.2.200`，绑定端口：`34568`，则
+    $python clip-pygtk.py --local ip:port --remote ip:port
+    #省略 port，默认为 34567
+    $python clip-pygtk.py --remote ip
+
+假设`电脑A`的内网IP为：`172.16.2.100`；`电脑B`的内网IP为：`172.16.2.200`，则
 
 在A上运行:
 
-    $python clipboard.py --local 172.16.2.100:34567 --remote 172.16.2.200:34568
+    $python clip-pygtk.py --remote 172.16.2.200
 
 在B上运行
 
-    $python clipboard.py --local 172.16.2.200:34568 --remote 172.16.2.100:34567
+    $python clip-pygtk.py --remote 172.16.2.100
 
-Just enjoy it ~~~
+默认绑定端口`34567`，如果出现端口冲突，可以指定其他端口：
+
+    $python clip-pygtk.py --local :18899 --remote 172.16.2.100:18899
--- clip-tkinter.py
+++ clip-tkinter.py
@@ -5,18 +5,12 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
 import time
-import StringIO
 import argparse
-import base64
 import threading
 import httplib
 import BaseHTTPServer
 import Queue
-
-try:
-    import gtk
-except ImportError:
-    import Tkinter as tk
+import Tkinter as tk
 
 
 """ 全局常量 """
@@ -46,7 +40,7 @@ def main():
     time.sleep(0.1)
 
     # 主线程，检测剪贴板数据
-    clipboard = Clipboard()
+    clipboard = ClipboardTK()
     clipboard.run()
 
 
@@ -61,50 +55,21 @@ def parse_cmdline():
     return args
 
 
-class Clipboard():
-    """ 剪贴板监听类 """
-
-    def __init__(self):
-        # 初始化剪贴板，优先使用gtk，如果该库不存在则使用python内置Tkinter
-        try:
-            self.clipboard = ClipboardGTK()
-            print 'Using PYGTK...'
-        except NameError:
-            self.clipboard = ClipboardTK()
-            print 'Using Tkinter...'
-
-        # 剪贴板数据
-        self.clipdata = ''
-
-    def run(self):
-        global RecvQueue, SendQueue
-        while True:
-            # 是否有数据发送过来
-            try:
-                mimetype, content = RecvQueue.get(timeout=1)
-                self.clipboard.set_content(mimetype, content)
-                self.clipdata = content
-            except Queue.Empty:
-                pass
-
-            # 剪贴板数据是否发生变化
-            mimetype, content = self.clipboard.get_content()
-            if content is not None and content != self.clipdata:
-                SendQueue.put((mimetype, content))
-                self.clipdata = content
-
-
 class ClientThread(threading.Thread):
     """ 数据发送线程 """
 
     def __init__(self, remote):
         threading.Thread.__init__(self)
         self.remote = remote
+        if ':' not in remote:
+            self.remote = '%s:%s' % (remote, SERVER_PORT)
+        else:
+            self.remote = remote
 
     def run(self):
         self.ping(self.remote)
         global SendQueue
-        mimetype, content, success = None, None, True
+        content, success = None, None, True
         while True:
             # 阻塞进程
             try:
@@ -165,11 +130,16 @@ class ServerThread(threading.Thread):
         self.local = local
 
     def run(self):
-        print "Staring Service on %s" % self.local
-        addr = self.local.split(':')[0]
-        port = int(self.local.split(':')[1])
-        server = BaseHTTPServer.HTTPServer((addr, port), RequestHandler)
-        server.serve_forever()
+        try:
+            addr_port = self.local.split(':')
+            addr = addr_port[0] if addr_port[0] != '' else '0.0.0.0'
+            port = int(addr_port[1]) if addr_port[1].isdigit() else SERVER_PORT
+            print "Staring Service on %s" % self.local
+            server = BaseHTTPServer.HTTPServer((addr, port), RequestHandler)
+            server.serve_forever()
+        except Exception as e:
+            print "Can't bind server on %s, Error: %s" % (self.local, e)
+            return
 
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -212,11 +182,12 @@ class ClipboardTK():
     python内置，仅支持剪贴板纯文本操作
     """
     def __init__(self):
+        print 'Using Tkinter...'
         self.root = tk.Tk()
         self.root.withdraw()
 
     def get_content(self):
-        self.root.after(10, self.get_text())
+        self.root.after(30, self.get_text())
         self.root.update()
         time.sleep(0.1)
         text = self.content
@@ -240,74 +211,24 @@ class ClipboardTK():
         self.root.clipboard_clear()
         self.root.clipboard_append(text)
 
+    def run(self):
+        """ Tkinter必须在主线程运行 """
+        clipdata = ''
+        global RecvQueue, SendQueue
+        while True:
+            # 是否有数据发送过来
+            try:
+                mimetype, content = RecvQueue.get(timeout=1)
+                self.set_content(mimetype, content)
+                clipdata = content
+            except Queue.Empty:
+                pass
 
-class ClipboardGTK():
-    """基于GTK的剪贴板类
-
-    需要安装pygtk，支持剪贴板纯文本，图像的操作
-    """
-    def __init__(self):
-        self.clipboard = gtk.Clipboard()
-        self.clipboard.clear()
-
-    def get_content(self):
-        """ 获取剪贴板内容，返回 CLIP_TYPTE, content """
-        text = self.get_text()
-        if text is not None:
-            return CLIP_TEXT, text
-
-        image = self.get_image()
-        if image is not None:
-            return CLIP_IMAGE, image
-
-        return CLIP_NONE, None
-
-    def set_content(self, mimetype, content):
-        if mimetype == CLIP_TEXT:
-            self.set_text(content)
-        elif mimetype == CLIP_IMAGE:
-            self.set_image(content)
-
-    def get_text(self):
-        content = None
-        if self.clipboard.wait_is_text_available():
-            content = self.clipboard.wait_for_text()
-
-        return content
-
-    def get_image(self):
-        content = None
-        if self.clipboard.wait_is_image_available():
-            pixbuf = self.clipboard.wait_for_image()
-            # Mac 下测试获取截图为空
-            if pixbuf is None:
-                pixbuf = self.clipboard.wait_for_contents('image/tiff').get_pixbuf()
-
-            content = self._pixbuf2b64(pixbuf)
-
-        return content
-
-    def set_text(self, text):
-        self.clipboard.set_text(text)
-        self.clipboard.store()
-
-    def set_image(self, b64_pixbuf):
-        pixbuf = self._b642pixbuf(b64_pixbuf)
-        self.clipboard.set_image(pixbuf)
-        self.clipboard.store()
-
-    def _pixbuf2b64(self, pixbuf):
-        """ gtk.gdk.Pixbuf对象转化为base64编码字符串 """
-        fh = StringIO.StringIO()
-        pixbuf.save_to_callback(fh.write, 'png')
-        return base64.b64encode(fh.getvalue())
-
-    def _b642pixbuf(self, b64):
-        """ base64编码字符串转换为gtk.gdk.Pixbuf """
-        pixloader = gtk.gdk.PixbufLoader('png')
-        pixloader.write(base64.b64decode(b64))
-        pixloader.close()
-        return pixloader.get_pixbuf()
+            # 剪贴板数据是否发生变化
+            mimetype, content = self.get_content()
+            if content is not None and content != clipdata:
+                SendQueue.put((mimetype, content))
+                clipdata = content
 
 
 if __name__ == "__main__":
